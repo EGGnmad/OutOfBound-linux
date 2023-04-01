cb7855a7448a1f560d228b32f9ed95827a9b20d8
EGGnmad <viewnono1219@gmail.com>
2023-04-01 11:01:32 +0900

    gitignore

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_video.h>
+
+using namespace std;
+
+struct Pos{
+    int x, y;
+};
+
+int main(){
+    SDL_Init(0);
+
+    SDL_Window* window = SDL_CreateWindow("Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+    SDL_Surface* canvas = SDL_GetWindowSurface(window);
+
+    bool isRunning = true;
+
+    while(isRunning){
+        memset(canvas->pixels, 255, 1280*720*4);
+        
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            switch (event.type) {
+                case SDL_QUIT:
+                    isRunning = false;
+            }
+
+        }
+        
+        SDL_UpdateWindowSurface(window);
+    }
+
+    return 0;
+}
