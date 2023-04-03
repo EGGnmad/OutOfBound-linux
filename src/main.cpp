9e5fb6a3c312309a3402ac71010dce87dcd7e124
ㅇㅂㅎ <viewnono1219@gmail.com>
2023-04-03 19:15:46 +0900

    +Board Image

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,39 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_surface.h>
-#include <SDL2/SDL_video.h>
-#include <SDL2/SDL_image.h>
 
-using namespace std;
-
-struct Pos{
-    int x, y;
-};
-
-int main(){
-    SDL_Init(0);
-
-    SDL_Window* window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
-    SDL_Surface* canvas = SDL_GetWindowSurface(window);
-
-    bool isRunning = true;
-
-    while(isRunning){
-        memset(canvas->pixels, 255, 1280*720*4);
-        
-        SDL_Event event;
-        while(SDL_PollEvent(&event)){
-            switch (event.type) {
-                case SDL_QUIT:
-                    isRunning = false;
-            }
-
+int main(int argc, char ** argv)
+{
+    bool quit = false;
+    SDL_Event event;
+ 
+    SDL_Init(SDL_INIT_VIDEO);
+ 
+    SDL_Window * window = SDL_CreateWindow("OutOfBound",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 720, 720, 0);
+ 
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Surface * image = SDL_LoadBMP("../img/board.bmp");
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+ 
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+ 
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                quit = true;
+                break;
         }
-        
-        SDL_UpdateWindowSurface(window);
-    }
 
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+ 
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(image);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+ 
+    SDL_Quit();
+ 
     return 0;
 }
